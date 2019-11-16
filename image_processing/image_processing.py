9cbf01e1a86224c663ae7d7fbd14b7f96c7add45
Michael Boyko <samaranin2912@gmail.com>
2019-11-16 14:00:00 +0200

    added some refactoring

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -51,7 +51,7 @@ def write_image_on_text(header='',
 
     content = Content(para, header_text, linkback, padding=top_padding)
 
-    temp_save_path = save_folder + ''.join(str(datetime.datetime.now()).split()) + '.png'
+    temp_save_path = get_save_file_name(save_folder)
     img = Image(content, width=width, height=height, fullpath=temp_save_path)
 
     img.draw_on_texture(source_path)
@@ -74,17 +74,18 @@ def write_image_on_text(header='',
 
     content = Content(para, header_text, linkback, padding=bottom_padding)
 
-    new_save_path = save_folder + ''.join(str(datetime.datetime.now()).split()) + '.png'
+    new_save_path = get_save_file_name(save_folder)
     img = Image(content, width=width, height=height, fullpath=new_save_path)
 
     img.draw_on_texture(temp_save_path)
 
     img = Img.open(new_save_path)
     img = img.crop((0, 0, width, height))
-    save_path = save_folder + ''.join(str(datetime.datetime.now()).split()) + '.png'
-    img.save(save_path)
 
-    return save_path
+    final_save_path = get_save_file_name(save_folder)
+    img.save(final_save_path)
+
+    return final_save_path
 
 
 def join_two_images(im1_path, im2_path, save_folder='temp/'):
@@ -110,4 +111,8 @@ def join_two_images(im1_path, im2_path, save_folder='temp/'):
 def get_list_of_files(my_path="temp/"):
     files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
     return sorted(files, key=lambda x: str(x[0]).lower())
+
+
+def get_save_file_name(save_folder="temp/"):
+    return save_folder + ''.join(str(datetime.datetime.now()).split()) + '.png'
 
